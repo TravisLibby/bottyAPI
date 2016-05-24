0077e4647656ad109d7c0b2cce5d59e3428687b2
Travis <travis.libby@xero.com>
2016-05-24 13:26:30 -0700

    Edit port number

--- index.js
+++ index.js
@@ -24,6 +24,6 @@ app.get('/trucks', function(req, res){
   });
 });
 
-app.listen('8081', function() {
+app.listen(process.env.PORT || 3000, function() {
   console.log('Listening on port 8081...'); 
 });
