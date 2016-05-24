4c56de6e5a1acffba07ced4279dc1dea55c1ce36
Travis <travis.libby@xero.com>
2016-05-24 12:56:35 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var request = require('request');
+var cheerio = require('cheerio');
+var app     = express();
+
+app.get('/trucks', function(req, res){
+
+  var url = 'http://offthegrid.com/wp-admin/admin-ajax.php?action=otg_market&delta=0&market=2&event=0';
+  
+  request(url, function(error, response, html){
+
+    if (!error){      
+      var $ = cheerio.load(html);
+      var trucksArr = [];  
+      var $trucks = $('.otg-market-data-vendors-names').first().children();
+
+      $trucks.each(function(index) {
+        trucksArr.push($(this).text());
+      });
+    }
+
+    res.send(trucksArr);
+  
+  });
+});
+
+app.listen('8081', function() {
+  console.log('Listening on port 8081...'); 
+});
