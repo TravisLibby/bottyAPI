d3016447989879c2ec21179dd0e2c5693a9b203b
Travis <travis.libby@xero.com>
2016-05-26 12:20:19 -0700

    Update endpoint.

--- index.js
+++ index.js
@@ -1,27 +1,21 @@
 var express = require('express');
 var request = require('request');
 var cheerio = require('cheerio');
-var app     = express();
+var app = express();
 
 app.get('/trucks', function(req, res){
 
-  var url = 'http://offthegrid.com/event/2/';
-  
-  request(url, function(error, response, html){
-
-    if (!error){      
-      var $ = cheerio.load(html);
-      var trucksArr = [];  
-      // var $trucks = $('.vendors-grid');
-
-      // $trucks.each(function(index) {
-      //   trucksArr.push($(this).text());
-      // });
-    }
-
-    res.send(html);
-  
+  var url = 'http://offthegrid.com/otg-api/passthrough/markets/2.json/';
+  var trucks = [];
+  request(url, function(err, data) {
+    console.log(typeof data);
+    data = JSON.parse(data.body);
+    data.MarketDetail.Events[0].Vendors.forEach(function(value, index) {
+      trucks.push(value.name);
+    });
+    res.send(trucks);
   });
+  
 });
 
 app.listen(process.env.PORT || 3000, function() {
